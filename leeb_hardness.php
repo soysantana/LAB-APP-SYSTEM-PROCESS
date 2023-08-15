47121a55d5eedc78412663acb51420ae236349cf
Santana <fsanttana@users.noreply.github.com>
2023-08-15 10:10:45 -0400

    ▶ feat: new-calculations

--- leeb_hardness.php
+++ leeb_hardness.php
@@ -1,7 +1,7 @@
 <?php
 $page_title = 'Leeb Hardness Test';
 require_once('includes/load.php');
-// Checkin What level user has permission to view this page
+// Verifica el nivel de permiso del usuario para ver esta página
 page_require_level(2);
 ?>
 
@@ -32,11 +32,8 @@ page_require_level(2);
         </strong>
       </div>
       <div class="panel-body">
-        <form method="post" action="#" onsubmit="calcular()">
+        <form method="post" action="#" oninput="calculateAverageStrength()">
 
-            <table class="table table-bordered">
-                <thead>
-          </div>
           <div class="col-xs-4">
             <label>Standard</label>
             <select class="form-control" name="Standard">
@@ -44,26 +41,27 @@ page_require_level(2);
               <option value="ASTM-A956">ASTM-A956</option>
             </select>
           </div>
+
           <div class="col-xs-4">
             <label>Method</label>
-            <select class="form-control" type = "text" name="method" id="">
+            <select class="form-control" type="text" name="method" id="">
               <option selected>Choose...</option>
               <option value="A">A</option>
               <option value="B">B</option>
               <option value="C">C</option>
             </select>
           </div>
-    
+
           <div class="col-xs-4">
             <label>Comments</label>
             <textarea class="form-control" name="Comments"></textarea>
           </div>
-    
+
           <div class="col-xs-4">
             <label>Technician</label>
             <input class="form-control" name="Technician" type="text">
           </div>
-    
+
           <div class="col-xs-4">
             <label>Test Start Date</label>
             <input class="form-control" name="Test_Start_Date" type="date">
@@ -71,87 +69,117 @@ page_require_level(2);
 
           <div class="panel-body">
             <div class="col-md-12">
+              <div style="display: flex; flex-direction: row; justify-content: space-between;">
+                <table class="table table-bordered border-primary" style="width:450px;">
+                  <caption>Testing Information</caption>
+                  <tbody>
+                    <tr>
+                      <th style="font-size: 15px; text-align: center; width: 350px; height: 25px;" colspan="2">Sample #:</th>
+                      <td><input type="text" style="border: none;" size="8" style="background: transparent;" id="1"></td>
+                    </tr>
+                    <tr>
+                      <th style="font-size: 15px; width: 350px; height: 25px;" scope="row">Depth (m)</th>
+                      <td><input type="text" style="border: none;" size="8" style="background: transparent;" id="2"></td>
+                      <td><input type="text" style="border: none;" size="8" style="background: transparent;" id="3"></td>
+                    </tr>
+                    <tr>
+                      <th style="font-size: 15px; width: 350px; height: 25px;" scope="row">Test No.</th>
+                      <th style="font-size: 15px; text-align: center; width: 350px; height: 25px;" colspan="2">Leeb Hardness Number</th>
+                    </tr>
+                    <tr>
+                      <th style="font-size: 15px; width: 350px; height: 25px;" scope="row">1</th>
+                      <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="No-1"></td>
+                    </tr>
+                    <tr>
+                      <th style="font-size: 15px; width: 350px; height: 25px;" scope="row">2</th>
+                      <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="No-2"></td>
+                    </tr>
+                    <tr>
+                      <th style="font-size: 15px; width: 350px; height: 25px;" scope="row">3</th>
+                      <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="No-3"></td>
+                    </tr>
+                    <tr>
+                      <th style="font-size: 15px; width: 350px; height: 25px;" scope="row">4</th>
+                      <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="No-4"></td>
+                    </tr>
+                    <tr>
+                      <th style="font-size: 15px; width: 350px; height: 25px;" scope="row">5</th>
+                      <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="No-5"></td>
+                    </tr>
+                    <tr>
+                      <th style="font-size: 15px; width: 350px; height: 25px;" scope="row">6</th>
+                      <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="No-6"></td>
+                    </tr>
+                    <tr>
+                      <th style="font-size: 15px; width: 350px; height: 25px;" scope="row">7</th>
+                      <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="No-7"></td>
+                    </tr>
+                    <tr>
+                      <th style="font-size: 15px; width: 350px; height: 25px;" scope="row">8</th>
+                      <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="No-8"></td>
+                    </tr>
+                    <tr>
+                      <th style="font-size: 15px; width: 350px; height: 25px;" scope="row">9</th>
+                      <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="No-9"></td>
+                    </tr>
+                    <tr>
+                      <th style="font-size: 15px; width: 350px; height: 25px;" scope="row">10</th>
+                      <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="No-10"></td>
+                    </tr>
+                    <tr>
+                      <th style="font-size: 15px; width: 350px; height: 25px;" scope="row">Average</th>
+                      <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="Average"></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
 
-          <div></div>
+<script>
+    function convertToNumberOrZero(value) {
+        const parsedValue = parseFloat(value);
+        return isNaN(parsedValue) ? 0 : parsedValue;
+    }
 
+    function calculateAverageStrength() {
+        const strengthInputs = [
+            document.getElementById('No-1'),
+            document.getElementById('No-2'),
+            document.getElementById('No-3'),
+            document.getElementById('No-4'),
+            document.getElementById('No-5'),
+            document.getElementById('No-6'),
+            document.getElementById('No-7'),
+            document.getElementById('No-8'),
+            document.getElementById('No-9'),
+            document.getElementById('No-10')
+        ];
 
-          <div style="display: flex; flex-direction: row; justify-content: space-between;">
-            <table class="table table-bordered border-primary" style="width:450px;">
-              <thead>
-              </thead>
-              <caption>Testing Information</caption>
-              <tbody>
-                <tr>
-                    <th style="font-size: 15px; text-align: center;" style="width: 350px; height: 25px;" scope="col" colspan="2">Sample #:</th>
-                    <td><input type="text" style="border: none;" size="8" style="background: transparent;" id="" oninput="calcular()"></td>
-                  </tr>
-                <tr>
-                  <th style="font-size: 15px;" style="width: 350px; height: 25px;" scope="row">Depth (m)</th>
-                  <td><input type="text" style="border: none;" size="8" style="background: transparent;" id="" oninput="calcular()"></td>
-                  <td><input type="text" style="border: none;" size="8" style="background: transparent;" id="" oninput="calcular()"></td>
-                </tr>
-                <tr>
-                <th style="font-size: 15px;" style="width: 350px; height: 25px;" scope="row">Test No.</th>
-                <th style="font-size: 15px; text-align: center;" style="width: 350px; height: 25px;" scope="col" colspan="2">Leeb Hardness Number</th>
-              </tr>
-              <tr>
-              <th style="font-size: 15px;" style="width: 350px; height: 25px;" scope="row">1</th>
-                <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="" oninput="calcular()"></td>
-              </tr>
-              <tr>
-              <th style="font-size: 15px;" style="width: 350px; height: 25px;" scope="row">2</th>
-              <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="" oninput="calcular()"></td>
-              </tr>
-              <tr>
-              <th style="font-size: 15px;" style="width: 350px; height: 25px;" scope="row">3</th>
-              <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="" oninput="calcular()"></td>
-              </tr>
-              <tr>
-              <th style="font-size: 15px;" style="width: 350px; height: 25px;" scope="row">4</th>
-              <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="" oninput="calcular()"></td>
-              </tr>
-              <tr>
-              <th style="font-size: 15px;" style="width: 350px; height: 25px;" scope="row">5</th>
-              <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="" oninput="calcular()"></td>
-            </tr>
-            <tr>
-                <th style="font-size: 15px;" style="width: 350px; height: 25px;" scope="row">6</th>
-                <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="" oninput="calcular()"></td>
-            </tr>
-            <tr>
-                <th style="font-size: 15px;" style="width: 350px; height: 25px;" scope="row">7</th>
-                <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="" oninput="calcular()"></td>
-            </tr>
-            <tr>
-                <th style="font-size: 15px;" style="width: 350px; height: 25px;" scope="row">8</th>
-                <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="" oninput="calcular()"></td>
-            </tr>
-            <tr>
-                <th style="font-size: 15px;" style="width: 350px; height: 25px;" scope="row">9</th>
-                <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="" oninput="calcular()"></td>
-            </tr>
-            <tr>
-                <th style="font-size: 15px;" style="width: 350px; height: 25px;" scope="row">10</th>
-                <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="" oninput="calcular()"></td>
-            </tr>
-            <tr>
-                <th style="font-size: 15px;" style="width: 350px; height: 25px;" scope="row">Average</th>
-                <td colspan="2"><input type="text" style="border: none;" size="35" style="background: transparent;" id="" oninput="calcular()"></td>
-            </tr>
-            </tbody>
-            </table>
-             </div>
+        let validCount = 0;
+        let totalStrength = 0;
 
+        for (const input of strengthInputs) {
+            const value = parseFloat(input.value);
+            if (!isNaN(value)) {
+                totalStrength += value;
+                validCount++;
+            }
+        }
 
+        if (validCount === 0) {
+            document.getElementById("14").value = 0;
+            return;
+        }
 
-
-
-
-
-        
-
-
-
+        const averageStrength = totalStrength / validCount;
+        document.getElementById("Average").value = isNaN(averageStrength) ? 0 : averageStrength.toFixed(2);
+    }
+</script>
+   
 <?php include_once('layouts/footer.php'); ?>
