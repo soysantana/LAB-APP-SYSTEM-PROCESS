c3dd6f21a717edae2bcfe65725019f48d4900e49
Santana <arturo.santtana@gmail.com>
2023-09-06 09:28:46 -0400

    📘 fix: correction-pages

--- consolidation.php
+++ consolidation.php
@@ -1,5 +1,5 @@
 <?php
-$page_title = 'Pinhole Test';
+$page_title = 'Consolidation';
 require_once('includes/load.php');
 // Checkin What level user has permission to view this page
 page_require_level(3);
