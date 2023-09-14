dfa3c54c11420f8c56c39905ab681e0e8860ba32
Wenjos <josedin@hotmail.com>
2023-09-13 22:12:43 -0400

    Actualizacion en inventario de muestras inalterada

--- inventario.php
+++ inventario.php
@@ -6,24 +6,19 @@ $products = join_lab_test_requisition_form();
 ?>
 
 <?php include_once('layouts/header.php'); ?>
-
 <div class="row">
     <div class="col-md-12">
         <div class="panel panel-default">
             <div class="panel-heading clearfix">
                 <strong>
                     <span class="glyphicon glyphicon-th"></span>
-                    <span>Menu de Inventario del Laboratorio</span>
+                    <span>Samples stored in List</span>
                 </strong>
             </div>
             <div class="panel-body">
-              <a href="inventarioequipos.php" class="btn btn-primary btn-lg btn-block">Inventarios de Equipos/Herramientas</a>
-              <a href="inventariosample.php" class="btn btn-primary btn-lg btn-block">Samples Inventory</a>
-             
-          </div>
-        </div>
-    </div>
-</div>
+               
+                <ul class="list-group">
+
 
 <?php include_once('layouts/footer.php'); ?>
 
