dce9857fb44283cfc9cee08a242d33073a8a3f2e
Wenjos <josedin@hotmail.com>
2023-09-22 16:12:44 -0400

    modificacion en el registro de muestras

--- ensayospendientes.php
+++ ensayospendientes.php
@@ -29,21 +29,18 @@ $sql = "SELECT
             WHERE lp.Sample_ID = p.Sample_ID
             AND lp.Sample_Number = p.Sample_Number
             AND lp.Test_Type = p.Test_Type
-            
         ) AND NOT EXISTS (
             SELECT 1 FROM muestra_en_realizacion r
             WHERE lp.Sample_ID = r.Sample_ID
             AND lp.Sample_Number = r.Sample_Number
             AND lp.Test_Type = r.Test_Type
-            
         ) AND NOT EXISTS (
             SELECT 1 FROM ensayo_en_entrega e
             WHERE lp.Sample_ID = e.Sample_ID
             AND lp.Sample_Number = e.Sample_Number
             AND lp.Test_Type = e.Test_Type
-           
-        )";
-
+        )
+        ORDER BY lp.Test_Type"; // Ordenar por el tipo de ensayo
 
 try {
     $result = $conn->query($sql);
@@ -54,26 +51,8 @@ try {
 
     include_once('layouts/header.php');
 
-    echo '<div class="row">';
-    echo '<div class="col-md-12">';
-    echo '<div class="panel panel-default">';
-    echo '<div class="panel-heading clearfix">';
-    echo '<strong>';
-    echo '<span class="glyphicon glyphicon-th"></span>';
-    echo '<span>Listado de ensayos pendientes</span>';
-    echo '</strong>';
-    echo '</div>';
-    echo '<div class="panel-body">';
-    echo '<table class="table">';
-    echo '<thead>';
-    echo '<tr>';
-    echo '<th>Fecha de Muestreo</th>';
-    echo '<th>ID Muestra</th>';
-    echo '<th>Número de Muestra</th>';
-    echo '<th>Ensayo</th>';
-    echo '</tr>';
-    echo '</thead>';
-    echo '<tbody>';
+    // Crear un array para almacenar los ensayos agrupados por tipo
+    $ensayos_agrupados = array();
 
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
@@ -82,25 +61,66 @@ try {
             $sample_date = $row['Sample_Date'];
             $testType = $row['Test_Type'];
 
-            if (!empty($testType)) {
-                echo "<tr>";
-                echo "<td>$sample_date</td>";
-                echo "<td>$sample_id</td>";
-                echo "<td>$sample_number</td>";
-                echo "<td>$testType</td>";
-                echo "</tr>";
+            // Verificar si el tipo de ensayo ya está en el array
+            if (!array_key_exists($testType, $ensayos_agrupados)) {
+                $ensayos_agrupados[$testType] = array();
             }
+
+            // Agregar el ensayo al tipo correspondiente
+            $ensayos_agrupados[$testType][] = array(
+                'Sample_ID' => $sample_id,
+                'Sample_Number' => $sample_number,
+                'Sample_Date' => $sample_date,
+            );
         }
-    } else {
-        echo "<tr>";
-        echo "<td colspan='4'>No hay ensayos pendientes.</td>";
-        echo "</tr>";
     }
 
-    echo '</tbody>';
-    echo '</table>';
-    echo '</div>';
-    echo '</div>';
+    echo '<div class="row">';
+    echo '<div class="col-md-12">';
+
+    // Iterar a través de los ensayos agrupados por tipo
+    foreach ($ensayos_agrupados as $testType => $ensayos) {
+        echo '<div class="panel panel-default">';
+        echo '<div class="panel-heading clearfix">';
+        echo "<strong>Tipo de Ensayo: $testType</strong>";
+        
+        // Mostrar la cantidad de ensayos pendientes junto al nombre del tipo de ensayo
+        $cantidad_pendiente = count($ensayos);
+        echo '<span class="badge pull-center">' . $cantidad_pendiente . '</span>';
+        
+        // Agregar un botón de impresión
+        echo '<button class="btn btn-primary btn-print pull-right" data-test-type="' . $testType . '">Imprimir</button>';
+        
+        echo '</div>';
+        echo '<div class="panel-body">';
+        echo '<table class="table">';
+        echo '<thead>';
+        echo '<tr>';
+        echo '<th>Fecha de Muestreo</th>';
+        echo '<th>ID Muestra</th>';
+        echo '<th>Número de Muestra</th>';
+        echo '</tr>';
+        echo '</thead>';
+        echo '<tbody>';
+
+        foreach ($ensayos as $ensayo) {
+            $sample_id = $ensayo['Sample_ID'];
+            $sample_number = $ensayo['Sample_Number'];
+            $sample_date = $ensayo['Sample_Date'];
+
+            echo "<tr>";
+            echo "<td>$sample_date</td>";
+            echo "<td>$sample_id</td>";
+            echo "<td>$sample_number</td>";
+            echo "</tr>";
+        }
+
+        echo '</tbody>';
+        echo '</table>';
+        echo '</div>';
+        echo '</div>';
+    }
+
     echo '</div>';
     echo '</div>';
 
@@ -112,3 +132,20 @@ try {
 // Cerrar la conexión a la base de datos
 $conn->close();
 ?>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    $(document).ready(function() {
+        // Manejar el clic en el botón de impresión
+        $('.btn-print').click(function() {
+            // Obtener el tipo de ensayo desde el atributo data
+            var testType = $(this).data('test-type');
+            
+            // Generar una URL de impresión basada en el tipo de ensayo
+            var printUrl = 'print.php?testType=' + testType;
+            
+            // Abrir una nueva ventana o pestaña para imprimir
+            window.open(printUrl, '_blank');
+        });
+    });
+</script>
