d0c22c0517c251c1c8cd7a97350601cbe1d5e113
Wenjos <josedin@hotmail.com>
2023-10-25 06:43:06 -0400

    Calculos de sanidad

--- soundness.php
+++ soundness.php
@@ -1620,52 +1620,52 @@ page_require_level(2);
 <script>
 
 function calcular() {
-
-    // obtenemos los valores de la parte gruesa
+ // obtenemos los valores de la parte gruesa
         
-    var WDS = parseFloat(document.getElementById("1").value);
-    var Ww = parseFloat(document.getElementById("2").value);
+    var WDS = parseFloat(document.getElementById("1").value)|| 0;
+    var Ww = parseFloat(document.getElementById("2").value)|| 0;
 
-    var WR4 = parseFloat(document.getElementById("4").value);
-    var WR3P5 = parseFloat(document.getElementById("7").value);
-    var WR3 = parseFloat(document.getElementById("10").value);
-    var WR2P5= parseFloat(document.getElementById("13").value);
-    var WR2 = parseFloat(document.getElementById("16").value);
-    var WR1P5 = parseFloat(document.getElementById("19").value);
-    var WR1 = parseFloat(document.getElementById("22").value);
-    var WR3P4 = parseFloat(document.getElementById("25").value);
-    var WR1P2= parseFloat(document.getElementById("28").value);
-    var WR3P8 = parseFloat(document.getElementById("31").value);
-    var WRN4 = parseFloat(document.getElementById("34").value);
+    var WR4 = parseFloat(document.getElementById("4").value)|| 0;
+    var WR3P5 = parseFloat(document.getElementById("7").value)|| 0;
+    var WR3 = parseFloat(document.getElementById("10").value)|| 0;
+    var WR2P5= parseFloat(document.getElementById("13").value)|| 0;
+    var WR2 = parseFloat(document.getElementById("16").value)|| 0;
+    var WR1P5 = parseFloat(document.getElementById("19").value)|| 0;
+    var WR1 = parseFloat(document.getElementById("22").value)|| 0;
+    var WR3P4 = parseFloat(document.getElementById("25").value)|| 0;
+    var WR1P2= parseFloat(document.getElementById("28").value)|| 0;
+    var WR3P8 = parseFloat(document.getElementById("31").value)|| 0;
+    var WRN4 = parseFloat(document.getElementById("34").value)|| 0;
 
-  // Calculamos los % Ret de la parte gruesa.
 
-  var TWRg = WR4 + WR3P5 + WR3 + WR2P5 + WR2 + WR1P5 + WR1 + WR3P4 + WR1P2 + WR3P8 + WRN4;
+// Calculamos los % Ret de la parte gruesa.
+
+var TWRg = WR4 + WR3P5 + WR3 + WR2P5 + WR2 + WR1P5 + WR1 + WR3P4 + WR1P2 + WR3P8 + WRN4;
    
-  var PR4 = ( WR4 / TWRg) * 100 ;
-  var PR3P5 = ( WR3P5 / TWRg) * 100 ;
-  var PR3 = ( WR3 / TWRg) * 100 ;
-  var PR2P5 = ( WR2P5 / TWRg) * 100 ;
-  var PR2 = ( WR2 / TWRg) * 100 ;
-  var PR1P5 = ( WR1P5 / TWRg) * 100 ;
-  var PR1 = ( WR1 / TWRg) * 100 ;
-  var PR3P4 = ( WR3P4 / TWRg) * 100 ;
-  var PR1P2 = ( WR1P2 / TWRg) * 100 ;
-  var PR3P8 = ( WR3P8 / TWRg) * 100 ;
-  var PRN4 = ( WRN4 / TWRg) * 100 ;
+   var PR4 = ( WR4 / TWRg) * 100 ;
+   var PR3P5 = ( WR3P5 / TWRg) * 100 ;
+   var PR3 = ( WR3 / TWRg) * 100 ;
+   var PR2P5 = ( WR2P5 / TWRg) * 100 ;
+   var PR2 = ( WR2 / TWRg) * 100 ;
+   var PR1P5 = ( WR1P5 / TWRg) * 100 ;
+   var PR1 = ( WR1 / TWRg) * 100 ;
+   var PR3P4 = ( WR3P4 / TWRg) * 100 ;
+   var PR1P2 = ( WR1P2 / TWRg) * 100 ;
+   var PR3P8 = ( WR3P8 / TWRg) * 100 ;
+   var PRN4 = ( WRN4 / TWRg) * 100 ;
+ 
+  var PTg = (TWRg / TWRg) * 100;
+ 
 
- var PTg = (TWRg / TWRg) * 100;
+// obtenemos los valores de la parte fina.
 
-
-  // obtenemos los valores de la parte fina.
-
-    var WRN4f = parseFloat(document.getElementById("40").value);
-    var WRN8 = parseFloat(document.getElementById("43").value);
-    var WRN16 = parseFloat(document.getElementById("46").value);
-    var WRN30 = parseFloat(document.getElementById("49").value);
-    var WRN50 = parseFloat(document.getElementById("52").value);
-    var WRN100 = parseFloat(document.getElementById("55").value);
-    var Wpan= parseFloat(document.getElementById("58").value);
+    var WRN4f = parseFloat(document.getElementById("40").value)|| 0;
+    var WRN8 = parseFloat(document.getElementById("43").value)|| 0;
+    var WRN16 = parseFloat(document.getElementById("46").value)|| 0;
+    var WRN30 = parseFloat(document.getElementById("49").value)|| 0;
+    var WRN50 = parseFloat(document.getElementById("52").value)|| 0;
+    var WRN100 = parseFloat(document.getElementById("55").value)|| 0;
+    var Wpan= parseFloat(document.getElementById("58").value)|| 0;
     
 
 
@@ -1683,116 +1683,160 @@ function calcular() {
 
    var PTf = pRN4f + pRN8 + pRN16 + pRN30 + pRN50 + pRN100 + pRpan;
 
-   
-  //SEGUNDA DISTRIBUCION
-  var TWRg1 = WR4 + WR3P5 + WR3 + WR2P5 + WR2 + WR1P5 + WR1 + WR3P4 + WR1P2 + WR3P8 + WRN4;
 
-    var r4 = WR4;
-    var r3P5 = WR3P5;
-    var r3 = WR3;
-    var r2P5=  WR2P5;
-    var r2 = WR2;
-    var r1P5 = WR1P5;
-    var r1 = WR1;
-    var r3P4 = WR3P4;
-    var r1P2= WR1P2;
-    var r3P8 = WR3P8; 
-    var rN4 = WRN4;
+   //SEGUNDA DISTRIBUCION
+var TWRg1 = WR4 + WR3P5 + WR3 + WR2P5 + WR2 + WR1P5 + WR1 + WR3P4 + WR1P2 + WR3P8 + WRN4;
 
-    var n4f = WRN4;
-    var n8 = WRN8;
-    var n16 = WRN16;
-    var n30 = WRN30;
-    var n50 = WRN50;
-    var n100 = WRN100;
-    var  pan = Wpan;
+var r4 = WR4;
+var r3P5 = WR3P5;
+var r3 = WR3;
+var r2P5=  WR2P5;
+var r2 = WR2;
+var r1P5 = WR1P5;
+var r1 = WR1;
+var r3P4 = WR3P4;
+var r1P2= WR1P2;
+var r3P8 = WR3P8; 
+var rN4 = WRN4;
+
+var n4f = WRN4;
+var n8 = WRN8;
+var n16 = WRN16;
+var n30 = WRN30;
+var n50 = WRN50;
+var n100 = WRN100;
+var  pan = Wpan;
 
 
-  var pR41 = ( WR4 / TWRg1) * 100 ;
-  var pR3P51 = ( WR3P5 / TWRg1) * 100 ;
-  var pR31 = ( WR3 / TWRg1) * 100 ;
-  var pR2P51 = ( WR2P5 / TWRg1) * 100 ;
-  var pR21 = ( WR2 / TWRg1) * 100 ;
-  var pR1P51 = ( WR1P5 / TWRg1) * 100 ;
-  var pR11 = ( WR1 / TWRg1) * 100 ;
-  var pR3P41 = ( WR3P4 / TWRg1) * 100 ;
-  var pR1P21 = ( WR1P2 / TWRg1) * 100 ;
-  var pR3P81 = ( WR3P8 / TWRg1) * 100 ;
-  var pRN41 = ( WRN4 / TWRg1) * 100 ;
-  
-  var PTg1 = (TWRg1 / TWRg1) * 100;
-  var TWRf1 =  n4f + n8 + n16+ n30+ n50+ n100+ pan;
+var pR41 = ( WR4 / TWRg1) * 100 ;
+var pR3P51 = ( WR3P5 / TWRg1) * 100 ;
+var pR31 = ( WR3 / TWRg1) * 100 ;
+var pR2P51 = ( WR2P5 / TWRg1) * 100 ;
+var pR21 = ( WR2 / TWRg1) * 100 ;
+var pR1P51 = ( WR1P5 / TWRg1) * 100 ;
+var pR11 = ( WR1 / TWRg1) * 100 ;
+var pR3P41 = ( WR3P4 / TWRg1) * 100 ;
+var pR1P21 = ( WR1P2 / TWRg1) * 100 ;
+var pR3P81 = ( WR3P8 / TWRg1) * 100 ;
+var pRN41 = ( WRN4 / TWRg1) * 100 ;
 
-   var pRN4f1 = ( WRN4 / WDS) * 100 ;
-   var pRN81 = ( WRN8/ WDS) * 100 ;
-   var pRN161 = ( WRN16 / WDS) * 100 ;
-   var pRN301 = ( WRN30 / WDS) * 100 ;
-   var pRN501 = ( WRN50 / WDS) * 100 ;
-   var pRN1001 = ( WRN100 / WDS) * 100 ;
-   var pRpan1 = ( Wpan / WDS) * 100 ;
+var PTg1 = (TWRg1 / TWRg1) * 100;
+var TWRf1 =  n4f + n8 + n16+ n30+ n50+ n100+ pan;
 
-   var PTf1 = pRN4f1 + pRN81 + pRN161 + pRN301 + pRN501 + pRN1001 + pRpan1;
+var pRN4f1 = ( WRN4 / WDS) * 100 ;
+var pRN81 = ( WRN8/ WDS) * 100 ;
+var pRN161 = ( WRN16 / WDS) * 100 ;
+var pRN301 = ( WRN30 / WDS) * 100 ;
+var pRN501 = ( WRN50 / WDS) * 100 ;
+var pRN1001 = ( WRN100 / WDS) * 100 ;
+var pRpan1 = ( Wpan / WDS) * 100 ;
 
-   // Fine Aggregate Soundness test
+var PTf1 = pRN4f1 + pRN81 + pRN161 + pRN301 + pRN501 + pRN1001 + pRpan1;
+
+// Fine Aggregate Soundness test
 
    // Start Weigth Ret.
 
-   var StwRN = parseFloat(document.getElementById("122.1").value);
-   var StwRN100  =  parseFloat(document.getElementById("125.1").value);
-   var StwRN50  =  parseFloat(document.getElementById("128.1").value);
-   var StwRN30  =  parseFloat(document.getElementById("131.1").value);
-   var StwRN16  =  parseFloat(document.getElementById("134.1").value);
-   var StwRN8  =  parseFloat(document.getElementById("137.1").value);
-    var StwRN4  =  parseFloat(document.getElementById("140.1").value);
+   var StwRN = parseFloat(document.getElementById("122.1").value)|| 0;
+   var StwRN100  =  parseFloat(document.getElementById("125.1").value)|| 0;
+   var StwRN50  =  parseFloat(document.getElementById("128.1").value)|| 0;
+   var StwRN30  =  parseFloat(document.getElementById("131.1").value)|| 0;
+   var StwRN16  =  parseFloat(document.getElementById("134.1").value)|| 0;
+   var StwRN8  =  parseFloat(document.getElementById("137.1").value)|| 0;
+    var StwRN4  =  parseFloat(document.getElementById("140.1").value)|| 0;
 
 
-  var FwRN =  parseFloat(document.getElementById("122").value);
-  var FwRN100 =  parseFloat(document.getElementById("125").value);
-  var FwRN50 =  parseFloat(document.getElementById("128").value);
-  var FwRN30 =  parseFloat(document.getElementById("131").value);
-  var FwRN16=  parseFloat(document.getElementById("134").value);
-  var FwRN8 =  parseFloat(document.getElementById("137").value);
-  var FwRN4 =  parseFloat(document.getElementById("140").value);
+  var FwRN =  parseFloat(document.getElementById("122").value)|| 0;
+  var FwRN100 =  parseFloat(document.getElementById("125").value)|| 0;
+  var FwRN50 =  parseFloat(document.getElementById("128").value)|| 0;
+  var FwRN30 =  parseFloat(document.getElementById("131").value)|| 0;
+  var FwRN16=  parseFloat(document.getElementById("134").value)|| 0;
+  var FwRN8 =  parseFloat(document.getElementById("137").value)|| 0;
+  var FwRN4 =  parseFloat(document.getElementById("140").value)|| 0;
    
-   var TfWr = FwRN + FwRN100 + FwRN50 + FwRN30 + FwRN16 + FwRN8 + FwRN4;
+   var TsWr = StwRN + StwRN100+ StwRN50 + StwRN30 + StwRN16 + StwRN8 + StwRN4;
+   var TfWrn = FwRN + FwRN100 + FwRN50 + FwRN30 + FwRN16 + FwRN8+ FwRN4;
+
+
+
+
+
+   // Porcentaje pasante designado.
     
-  var PPdn = ((StwRN - FwRN)/ StwRN)* 100;
-  var PPdn100 = ((StwRN100 - FwRN100)/ StwRN100)* 100;
-  var PPdn50 = ((StwRN50 - FwRN50)/ StwRN50)* 100;
-  var PPdn30 = ((StwRN30 - FwRN30)/ StwRN30)* 100;
-  var PPdn16 = ((StwRN16 - FwRN16)/ StwRN16)* 100;
-  var PPdn8 = ((StwRN8 - FwRN8)/ StwRN8)* 100;
-  var PPdn4 = ((StwRN4 - FwRN4)/ StwRN4)* 100;
+  var PPdn;
+  var PPdn100;
+  var PPdn50;
+  var PPdn30;
+  var PPdn16;
+  var PPdn8;
+  var PPdn4;
 
 
+  
+  if (PPdn === ""){
+    PPdn = "";
+  }else{
+    PPdn= ((StwRN - FwRN)/ StwRN)* 100;
+  }
+
+  if (PPdn100 === ""){
+    PPdn100 = "";
+  }else{
+    PPdn100=  ((StwRN100 - FwRN100)/ StwRN100)* 100;
+  }
+
+  if (PPdn50=== ""){
+    PPdn50 = "";
+  }else{
+    PPdn50=  ((StwRN50 - FwRN50)/ StwRN50)* 100;
+  }
+
+  if (PPdn30=== ""){
+    PPdn30 = "";
+  }else{
+    PPdn30=  ((StwRN30 - FwRN30)/ StwRN30)* 100;
+  }
+
+  if (PPdn16=== ""){
+    PPdn16 = "";
+  }else{
+    PPdn16= ((StwRN16 - FwRN16)/ StwRN16)* 100;
+  }
+
+  if (PPdn8 === ""){
+    PPdn8 = "";
+  }else{
+    PPdn8 = ((StwRN8 - FwRN8)/ StwRN8)* 100;
+  }
+
+  if (PPdn4 === ""){
+    PPdn4 = "";
+  }else{
+    PPdn4 = ((StwRN4 - FwRN4)/ StwRN4)* 100;
+  }
+  
   var WpMn100 = (PPdn100 * pRN100) / 100;
   var WpMn50 = (PPdn50 * pRN50) / 100;
   var WpMn30 = (PPdn30 * pRN30) / 100;
   var WpMn16 = (PPdn16 * pRN16) / 100;
   var WpMn8 = (PPdn8 * pRN8) / 100;
-  var WpMn4 = (PPdn4 * pRN41) / 100;
+  var WpMn4 = (PPdn4 * pRN4f) / 100;
 
 
- var TWpM = WpMn100 + WpMn50 + WpMn30 + WpMn16 + WpMn8 + WpMn4;
+ 
+var valores = [WpMn100, WpMn50, WpMn30, WpMn16, WpMn8, WpMn4 ];
 
+// La condición que deseas aplicar (por ejemplo, mayor o igual a 0)
+var condicion = 0;
 
- //obtenemos los valores de la parte fina de los resultados.
+var suma = 0;
 
- var swrN200 = parseFloat(document.getElementById("122.1").value);
- var swrN100 = parseFloat(document.getElementById("125.1").value);
- var swrN50 = parseFloat(document.getElementById("128.1").value);
- var swrN30 = parseFloat(document.getElementById("131.1").value);
- var swrN16 = parseFloat(document.getElementById("134.1").value);
- var swrN8 = parseFloat(document.getElementById("137.1").value);
- var swrN4 = parseFloat(document.getElementById("140.1").value);
-
- var tswr = swrN200 + swrN100 + swrN50 + swrN30 + swrN16 + swrN8 + swrN4;
-
-
-
-
-
-      // pasamos los resultados a los inputs
+for (var i = 0; i < valores.length; i++) {
+  if (valores[i] >= condicion) {
+    suma += valores[i];
+  }
+}
+    // pasamos los resultados a los inputs
 
 document.getElementById("37").value = TWRg.toFixed(2);
 document.getElementById("38").value = PTg.toFixed(2);
@@ -1811,7 +1855,7 @@ document.getElementById("35").value = PRN4.toFixed(2);
 
 document.getElementById("41").value = pRN4f.toFixed(2);
 document.getElementById("44").value = pRN8.toFixed(2);
-document.getElementById("47").value = pRN16  .toFixed(2);
+document.getElementById("47").value = pRN16.toFixed(2);
 document.getElementById("50").value = pRN30.toFixed(2);
 document.getElementById("53").value = pRN50.toFixed(2);
 document.getElementById("56").value = pRN100.toFixed(2);
@@ -1875,14 +1919,25 @@ document.getElementById("119").value = pRpan1.toFixed(2);
 document.getElementById("120").value = TWRf1.toFixed(2);
 document.getElementById("121").value = PTf1.toFixed(2);
 
-document.getElementById("123").value = PPnd.toFixed(2);
-document.getElementById("126").value = PPnd100.toFixed(2);
-document.getElementById("129").value = PPnd50.toFixed(2);
+document.getElementById("123").value = PPdn.toFixed(2);
+document.getElementById("126").value = PPdn100.toFixed(2);
+document.getElementById("129").value = PPdn50.toFixed(2);
+document.getElementById("132").value = PPdn30.toFixed(2);
+document.getElementById("135").value = PPdn16.toFixed(2);
+document.getElementById("138").value = PPdn8.toFixed(2);
+document.getElementById("141").value = PPdn4.toFixed(2);
+document.getElementById("143.1").value = TsWr.toFixed(2);
+document.getElementById("143").value = TfWrn.toFixed(2);
 
-document.getElementById("143.1").value = tswr.toFixed(2);
+document.getElementById("127").value = WpMn100.toFixed(2);
+document.getElementById("130").value = WpMn50.toFixed(2);
+document.getElementById("133").value = WpMn30.toFixed(2);
+document.getElementById("136").value = WpMn16.toFixed(2);
+document.getElementById("139").value = WpMn8.toFixed(2);
+document.getElementById("142").value = WpMn4.toFixed(2);
 
-
-
+document.getElementById("145").value = suma;
+    
 }
 </script>
 
