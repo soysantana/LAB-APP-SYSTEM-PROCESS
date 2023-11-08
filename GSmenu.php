254ecd6e4c55667057e471fe08c524ef89b66409
Santana <arturo.santtana@gmail.com>
2023-11-08 09:23:59 -0400

    🎉 correction-funcionamientos

--- GSmenu.php
+++ GSmenu.php
@@ -9,27 +9,27 @@ $products = join_lab_test_requisition_form();
 <?php include_once('layouts/header.php'); ?>
 
 <div class="row">
-    <div class="col-md-12">
-        <div class="panel panel-default">
-            <div class="panel-heading clearfix">
-                <strong>
-                    <span class="glyphicon glyphicon-th"></span>
-                    <span>Menu de Ensayos de Granulometrias</span>
-                </strong>
-            </div>
-            <div class="panel-body">
-                <div class="list-group">
-              <a href="Grain_Size.php" class="list-group-item">Granulometria por Tamizado para Suelos</a>
-              <a href="GSFull.php" class="list-group-item">Granulometria por Tamizado para Suelos Full</a>
-              <a href="gs_fine_agg.php" class="list-group-item">Granulometria por Tamizado de Agregados Finos</a>
-              <a href="gs_coarse_agg.php" class="list-group-item">Granulometria por Tamizado de Agregados Gruesos</a>
-              <a href="gs_coarsethan_agg.php" class="list-group-item">Granulometria por Tamizado de Agregados mas Gruesos</a>
-              <a href="gs_rock.php" class="list-group-item">Granulometria por Tamizado de Rocas</a>
-              <a href="hydrometer.php" class="list-group-item">Granulometria por Sedimentacion</a>
-            </div>
+  <div class="col-md-12">
+    <div class="panel panel-default">
+      <div class="panel-heading clearfix">
+        <strong>
+          <span class="glyphicon glyphicon-th"></span>
+          <span>Menu de Ensayos de Granulometrias</span>
+        </strong>
+      </div>
+      <div class="panel-body">
+        <div class="list-group">
+          <a href="Grain_Size.php" class="list-group-item">Granulometria por Tamizado para Suelos</a>
+          <a href="GSFull.php" class="list-group-item">Granulometria por Tamizado para Suelos Full</a>
+          <a href="gs_fine_agg.php" class="list-group-item">Granulometria por Tamizado de Agregados Finos</a>
+          <a href="gs_coarse_agg.php" class="list-group-item">Granulometria por Tamizado de Agregados Gruesos</a>
+          <a href="gs_coarsethan_agg.php" class="list-group-item">Granulometria por Tamizado de Agregados mas Gruesos</a>
+          <a href="gs_rock.php" class="list-group-item">Granulometria por Tamizado de Rocas</a>
+          <a href="hydrometer.php" class="list-group-item">Granulometria por Sedimentacion</a>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 </div>
 
 <?php include_once('layouts/footer.php'); ?>
