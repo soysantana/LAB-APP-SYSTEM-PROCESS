27fc9be6958ab2a2e73cffdbea38e43106e21a8e
Santana <arturo.santtana@gmail.com>
2023-11-28 09:29:14 -0400

    ⚡ funcion para enviar a repetir

--- db/SgFine.php
+++ db/SgFine.php
@@ -233,3 +233,48 @@ $Specific_Gravity = find_by_id('specific_gravity_fine', (int)$_GET['id']);
     }
   }
 ?>
+
+<?php
+// Verifica si se ha hecho clic en el botón "repetir"
+if (isset($_POST['repetir_muestra'])) {
+    // Obtén el ID de la muestra específica
+    $search_table = (int)$_GET['id'];
+    $reportdate = make_date();
+
+    // Consulta para obtener los datos de la muestra específica
+    $query_select = "SELECT * FROM specific_gravity_fine WHERE id = '{$search_table}'";
+    $result_select = $db->query($query_select);
+
+    if ($result_select && $db->num_rows($result_select) == 1) {
+        $search_table_data = $db->fetch_assoc($result_select);
+
+        // Verifica si ya existe una fila con los mismos Sample_ID, Sample_Number y Test_Type
+        $query_check_existence = "SELECT COUNT(*) as count FROM ensayo_en_repeticion WHERE Sample_ID = '{$search_table_data['Sample_ID']}' AND Sample_Number = '{$search_table_data['Sample_Number']}' AND Test_Type = '{$search_table_data['test_type']}'";
+        $result_check_existence = $db->query($query_check_existence);
+        $existence_data = $db->fetch_assoc($result_check_existence);
+
+        if ($existence_data['count'] > 0) {
+            // Ya existe una fila con los mismos Sample_ID, Sample_Number y Test_Type
+            $session->msg('d', 'No se puede repetir la muestra porque ya existe una fila con los mismos Sample_ID, Sample_Number y Test_Type.');
+            redirect('repiteEnsayo.php', false);
+        } else {
+            // Inserta los datos en la tabla "ensayo_en_repeticion"
+            $query_insert = "INSERT INTO ensayo_en_repeticion (Sample_ID, Sample_Number, Test_Type, Tecnico, Fecha_Inicio) VALUES ";
+            $query_insert .= "('{$search_table_data['Sample_ID']}', '{$search_table_data['Sample_Number']}', '{$search_table_data['test_type']}', '{$search_table_data['Technician']}', '{$reportdate}')";
+
+            $result_insert = $db->query($query_insert);
+
+            if ($result_insert && $db->affected_rows() === 1) {
+                $session->msg('s', 'Muestra repetida con éxito.');
+                redirect('repiteEnsayo.php', false);
+            } else {
+                $session->msg('d', 'Error al repetir la muestra.');
+                redirect('repiteEnsayo.php', false);
+            }
+        }
+    } else {
+        $session->msg('d', 'Error al obtener datos de la muestra específica.');
+        redirect('repiteEnsayo.php', false);
+    }
+}
+?>
